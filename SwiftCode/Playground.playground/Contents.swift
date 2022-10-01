5a967a42cc314021cfc9b0d5d133af14814ea591
Pavel Chvyrov <pkchvyrov@vk.com>
2022-10-01 03:34:45 +0300

    add closure sort

--- SwiftCode/Playground.playground/Contents.swift
+++ SwiftCode/Playground.playground/Contents.swift
@@ -1,3 +1,26 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+//: Написать сортировку массива с помощью замыкания, сначала в одну сторону, затем в другую.Вывести всё в консоль.
+
+
+
+var arr : [Int] = []
+
+for i in 0..<10 {
+    arr.append(Int.random(in: 0..<100))
+}
+
+
+//var sortedAscending = arr.sorted(by: { (first: Int, second: Int) -> Bool in
+//    return first > second
+//})
+
+var sortedDescending = arr.sorted(by: { (first: Int, second: Int) -> Bool in
+    return second > first
+})
+
+print(arr)
+print("\n\(sortedDescending) Отсортированный по возрастанию")
+//print("\n\(sortedAscending) Отсортированный по убыванию")
+
